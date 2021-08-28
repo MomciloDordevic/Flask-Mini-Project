ff4aa1e7384ce83fb7403feae715085a467bb558
MomciloDordevic <qwertyumom@gmail.com>
2021-08-28 13:44:37 +0000

    initial commit

---
+++ evn.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "QeC24*ouA{iu|yW&EF}s")
+os.environ.setdefault("MONGO_URI", "")
+os.environ.setdefault("MONGO_DBNAME", "task_manager")
